24b5847ca7b35973b2aac6573347b93d180fcbf0
shooontan <logo.bee.doo19@gmail.com>
2019-10-30 17:32:55 +0900

    add async await flow

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -7,12 +7,29 @@ const Layout = loadable(() => import('~/client/layouts/default'));
 interface IndexPageProps {}
 
 const IndexPage: React.FC<IndexPageProps> = () => {
+  const [date, setDate] = React.useState<string>();
+
+  // async await flow
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const res: { request: boolean; time: string } = await fetch(
+          '/api'
+        ).then(res => res.json());
+        setDate(res.time);
+      } catch (_) {
+        //
+      }
+    })();
+  }, []);
+
   return (
     <Layout>
       <Helmet>
         <title>index</title>
       </Helmet>
       <p>Index Page</p>
+      {date ? date : 'loading...'}
     </Layout>
   );
 };
