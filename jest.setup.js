4d93b60a618af3a51413949e80aa3447ccdc67b2
shooontan <logo.bee.doo19@gmail.com>
2019-10-31 10:30:00 +0900

    update snapshot

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,14 @@
 require('jest-styled-components');
+
+global.beforeEach(() => {
+  // for Link Component
+  global.IntersectionObserver = class IntersectionObserver {
+    observe() {
+      return null;
+    }
+
+    unobserve() {
+      return null;
+    }
+  };
+});
